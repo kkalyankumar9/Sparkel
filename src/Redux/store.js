289b332b8a2fd4562765a680b5f04ebc8435474e
kkalyankumar9 <kummarikalyan010@gmail.com>
2023-07-18 23:26:22 +0530

    master

---
+++ src/Redux/store.js
@@ -0,0 +1,4 @@
+import { applyMiddleware, legacy_createStore,combineReducers } from "redux";
+import thunk from "redux-thunk";
+const rootReducer=combineReducers({})
+export const store= legacy_createStore(rootReducer,applyMiddleware(thunk))
