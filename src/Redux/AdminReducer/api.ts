9ad5ed31a3c50dfed20842fe4d4ecb45a2a7719d
Ankit Kumar <ankitkrai4n0522@gmail.com>
2023-07-20 00:57:46 +0530

    AdminReducer

---
+++ src/Redux/AdminReducer/api.ts
@@ -0,0 +1,20 @@
+// import axios from "axios";
+
+// import { NewDataType } from "../../Pages/AdminPages";
+
+// export const addProduct = (newData:NewDataType) => {
+// axios.post(`https://sparkel.onrender.com/data`,newData).then((res)=>{
+//     return(res.data)
+//    }).catch((err)=>{
+//     console.log(err)
+//    })
+// };
+
+// export const deleteProduct = async (id: number) => {
+//     try {
+//       const response = await axios.delete(`https://sparkel.onrender.com/data/${id}`);
+//       return response.data;
+//     } catch (error) {
+//       throw new Error(`Failed to delete product: ${error}`);
+//     }
+//   };
